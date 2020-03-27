0915ee7e5a94593d4c6aff71c4fa6b879f5f584f
charli <charlyoleg@fabfolk.com>
2020-03-27 18:32:56 +0100

    add library code

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+/*! index.ts */
+
+export function add1000(x: number): number {
+  let res: number = x;
+  res += 1000;
+  return res;
+}
+
+export function mult1000(x: number): number {
+  let res: number = x;
+  res *= 1000;
+  return res;
+}
