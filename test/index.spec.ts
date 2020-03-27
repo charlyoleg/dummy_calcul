e7de256bfffcb6d6fb4478d9a1272be7cd2e1214
charli <charlyoleg@fabfolk.com>
2020-03-27 18:49:45 +0100

    add tests

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -6,6 +6,14 @@ test("add1000", () => {
   expect(add1000(5)).toEqual(1005);
 });
 
+test("add1056", () => {
+  expect(add1000(56)).toEqual(1056);
+});
+
 test("mult1000", () => {
   expect(mult1000(5)).toEqual(5000);
+});
+
+test("mult1056", () => {
+  expect(mult1000(5.6)).toEqual(5600);
 });
