e6ebf1462db4a0b063e39c844fae1704f23f9303
charli <charlyoleg@fabfolk.com>
2020-04-09 21:06:10 +0200

    adding a sub-module to the source

---
+++ src/calcul_sub.ts
@@ -0,0 +1,11 @@
+/* calcul_sub.ts */
+
+function addi500(x: number): number {
+  let r: number = 500;
+  r += x;
+  return r
+}
+
+// export the function addi500
+export { addi500 };
+
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 /*! index.ts */
 
+import {addi500} from './calcul_sub';
+
 export function add1000(x: number): number {
-  let res: number = x;
-  res += 1000;
+  let res: number;
+  res = addi500(x);
+  res += 500;
   return res;
 }
 
