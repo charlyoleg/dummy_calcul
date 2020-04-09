fc6aede27cf753316e2b44cae873dbe2671a942e
charli <charlyoleg@fabfolk.com>
2020-04-10 01:25:57 +0200

    starting case_4

---
+++ using_the_npm_package/case_4/src/abc.ts
@@ -0,0 +1,7 @@
+// abc.ts
+
+import * as dummCalculy from 'dummy-calcul';
+
+console.log('one: ' + dummCalculy.add1000(15));
+console.log('two: ' + dummCalculy.mult1000(9));
+console.log('End of abc.ts');
