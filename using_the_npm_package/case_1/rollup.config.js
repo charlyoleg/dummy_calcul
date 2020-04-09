5bab5ff3a7c5d15adc1196ca12d06c69ea996517
charli <charlyoleg@fabfolk.com>
2020-04-10 00:38:42 +0200

    case_1 marche

--- using_the_npm_package/case_1/rollup.config.js
+++ using_the_npm_package/case_1/rollup.config.js
@@ -2,7 +2,7 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 
 export default {
-  input: 'src/main.js',
+  input: 'src/abc.js',
   output: {
     file: 'dist/bundle.js',
     format: 'iife'
